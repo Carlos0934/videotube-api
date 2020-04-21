51be7b851a9a4a66f1e0a454ff46d7fcfe4d5964
Carlos Engel <carlosengel011@gmail.com>
2020-04-21 02:12:57 -0400

    UserStorage  Update and delete added

--- models/user.go
+++ models/user.go
@@ -63,8 +63,26 @@ func (storage *UserStorage) Save(data interface{}) error {
 
 func (storage *UserStorage) Update(condition map[string]string, data interface{}) error {
 
+	if user, ok := data.(User); ok {
+		stmt, err := storage.conn.Prepare("UPDATE users SET  username = ? email = ? password = ?  birthdate, ? WHERE ? ")
+		CheckError(err)
+		result, err := stmt.Exec(user.Username, user.Email, user.Password, user.Birthdate, condition["id"])
+		CheckError(err)
+
+		_, err = result.LastInsertId()
+		CheckError(err)
+
+	}
+
+	return nil
 }
 
 func (storage *UserStorage) Delete(condition map[string]string) bool {
+	stmt, err := storage.conn.Prepare("DELETE FROM users WHERE id = ?")
+	CheckError(err)
+	result, err := stmt.Exec(condition["id"])
+	count, err := result.RowsAffected()
+	CheckError(err)
 
+	return count > 0
 }
