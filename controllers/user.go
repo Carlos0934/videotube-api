930d5559d2723ed990ec47e39584248b284f0617
Carlos Engel <carlosengel011@gmail.com>
2020-04-22 21:44:41 -0400

    UserController Get

--- controllers/user.go
+++ controllers/user.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/carlos0934/videotube/models"
+	"github.com/gorilla/mux"
 )
 
 type UserController struct {
@@ -17,6 +20,20 @@ func NewUserController() *UserController {
 }
 
 func (controller *UserController) Get(w http.ResponseWriter, r *http.Request) {
+
+	id := mux.Vars(r)["user"]
+	filter := map[string]interface{}{"id": id}
+	user := models.User{}
+	err := controller.storage.FindOne(filter, &user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	data, err := json.Marshal(&user)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(data)
 
 }
 
